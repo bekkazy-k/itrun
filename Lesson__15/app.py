e0d02a3872d03377d4dee0da3b2232afaa888d35
Бекказы Кубанычбеков <bekkazy.k@gmail.com>
2021-03-09 22:05:57 +0600

    Lesson 15

---
+++ Lesson__15/app.py
@@ -0,0 +1,14 @@
+from base import Pawn, Elephant
+
+# p = Pawn('black', 1, 2)
+# print(p.move(1, 4)) # True
+# print(p.get_position_x())
+# print(p.get_position_y())
+# print(p.move(1, 6)) # True
+# print(p.get_position_x())
+# print(p.get_position_y())
+
+e = Elephant("Black", 3, 1)
+print(e.move(5, 3))  # True
+print(e.get_position_x())
+print(e.get_position_y())
--- Lesson__15/base.py
+++ Lesson__15/base.py
@@ -22,11 +22,54 @@ class BaseFigure(ABC):
 
 
 class Pawn(BaseFigure):
-    def move(self, position):
+    def move(self, x, y):
+        """
+        1 - x должен быть тот же
+        2 - y должен повышаться на 1 или на 2 в первый ход
+        3 - поменять позиции на новые
+        """
+        if x != self.x:
+            return False
+
+        elif self.y == 2:
+            if y - self.y > 2 or y == self.y:
+                return False
+        elif y - self.y > 1:
+            return False
+        self.y = y
+        self.x = x
+        return True
+
+
+class Elephant(BaseFigure):
+    def move(self, x, y):
+        if self.x == x:
+            return False
+
+        if abs(self.x - x) == abs(self.y - y):
+            self.y = y
+            self.x = x
+            return True
+
+        return False
+
+
+class Human(ABC):
+    @abstractmethod
+    def greet(self):
         pass
 
 
-p = Pawn('black', 'a2')
-p.move('a3') # True
-p.move('b3') # False
-print(p.get_position())
+class Kyrgyz(Human):
+    def greet(self):
+        print("Саламалекум")
+
+
+class Russian(Human):
+    def greet(self):
+        print("Привет")
+
+k = Kyrgyz()
+k.greet()
+r = Russian()
+r.greet()
