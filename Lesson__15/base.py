4edef0e957bc4ae520d6b8a6358424c7a684576b
Бекказы Кубанычбеков <bekkazy.k@gmail.com>
2021-03-09 20:45:58 +0600

    Lesson 15

--- Lesson__15/base.py
+++ Lesson__15/base.py
@@ -2,19 +2,31 @@ from abc import ABC, abstractmethod
 
 
 class BaseFigure(ABC):
-    def __init__(self, color, position):
+    def __init__(self, color, x, y):
         self.color = color
-        self._position = position
+        self.x = x
+        self.y = y
 
     def draw(self):
         print("Draw figure on position:", self.color)
-    
+
     @abstractmethod
+    def move(self, x, y):
+        pass
+
+    def get_position_x(self):
+        return self.x
+
+    def get_position_y(self):
+        return self.y
+
+
+class Pawn(BaseFigure):
     def move(self, position):
         pass
 
-    def get_position(self):
-        return self._position
 
-
-print("♕ ♖ ♗ ♘ ♙ ♚ ♛ ♜ ♝ ♞ ♟ ♠")
+p = Pawn('black', 'a2')
+p.move('a3') # True
+p.move('b3') # False
+print(p.get_position())
