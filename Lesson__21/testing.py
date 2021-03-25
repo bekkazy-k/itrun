7fcc7e49ae0c6ca3e7e04ccade7f5166907a0fbc
Бекказы Кубанычбеков <bekkazy.k@gmail.com>
2021-03-25 21:01:13 +0600

    Update testing.py

--- Lesson__21/testing.py
+++ Lesson__21/testing.py
@@ -13,6 +13,18 @@ class TestSumFunc(unittest.TestCase):
     def test_str_all(self):
         self.assertEqual(sum_func('5', '6'), 11)
 
+    def test_bool_true(self):
+        self.assertEqual(sum_func(5, True), False)
+
+    def test_bool_false(self):
+        self.assertEqual(sum_func(5, False), False)
+
+    def test_list(self):
+        self.assertEqual(sum_func(5, []), False)
+
+    def test_dict(self):
+        self.assertEqual(sum_func(5, {}), False)
+
 
 if __name__ == '__main__':
     unittest.main()
