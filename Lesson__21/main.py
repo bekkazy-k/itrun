a435386345a328f942dc2da66eaaed9557dd56f3
Daniiar <akimovdaniiar010@gmail.com>
2021-03-25 20:26:54 +0600

    Update main.py

--- Lesson__21/main.py
+++ Lesson__21/main.py
@@ -2,3 +2,11 @@
 Написать функцию sum_func, которая принимает 2 параметра и возвращает их сумму.
 Обработать все возможные ошибки
 """
+
+def sum_func(x, y):
+    if x or y == int:
+        return x + y
+    else:
+        return False
+
+print(sum_func(5, 10))
