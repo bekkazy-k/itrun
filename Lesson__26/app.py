3db3763504f511d330cfdca49a7b4cedb4b2f9d3
Daniiar <akimovdaniiar010@gmail.com>
2021-04-08 20:22:05 +0600

    Update app.py

--- Lesson__26/app.py
+++ Lesson__26/app.py
@@ -13,7 +13,7 @@ and they lived at the bottom of a well.</p>
 <p class="story">...</p>
 """
 
-soup = BeautifulSoup(html_doc, "html.parser")
+# soup = BeautifulSoup(html_doc, "html.parser")
 
 # print(soup.prettify())
 # print(soup.title)
@@ -24,3 +24,59 @@ soup = BeautifulSoup(html_doc, "html.parser")
 # print(soup.p['class'])
 # print(soup.find_all('a'))
 # print(soup.find(id="link3"))
+
+# from bs4 import BeautifulSoup
+# import requests
+# url = ('https://pypi.org/project/beautifulsoup4/')
+# r = requests.get(url)
+#
+# soup = BeautifulSoup(r.text, "html.parser")
+#
+# print(soup.h2['class'])
+# # print(soup.h2.string)
+# print(soup.div['class'])
+# print(soup.find_all("div", {"class": "project-description"})[0].p.string)
+
+# from bs4 import BeautifulSoup
+# import requests
+# url = ("https://github.com/Daniiar070?tab=repositories")
+# r = requests.get(url)
+# soup = BeautifulSoup(r.text, "html.parser")
+#
+# print(soup.h3['class'])
+# print(soup.find_all("li", {"class"}))
+# print(soup.find_all("h3"))
+# print(soup.find_all("a"))
+
+# print(soup.find_all("div", {"class": "project-description"})[0].p.string)
+# print(soup.find_all("h3", {"class": "wb-break-all"})[0].a.string)
+# print(soup.find_all("h3", {"class": "wb-break-all"})[1].a.string)
+# print(soup.find_all("h3", {"class": "wb-break-all"})[2].a.string)
+# print(soup.find_all("h3", {"class": "wb-break-all"})[3].a.string)
+# from bs4 import BeautifulSoup
+# import requests
+# url = ("https://github.com/Daniiar070")
+# r = requests.get(url)
+# soup = BeautifulSoup(r.text, "html.parser")
+# from bs4 import BeautifulSoup
+# import requests
+# url = ("https://github.com/Daniiar070?tab=repositories")
+# r = requests.get(url)
+# soup = BeautifulSoup(r.text, "html.parser")
+# p = input("Введите логин: ")
+# print(f"https://github.com/{p}?tab=repositories")
+# if p == "Daniiar070":
+#     for i in soup.find_all("h3", {"class": "wb-break-all"}):
+#         print(i.a.string.replace(" ", ""), end="")
+
+
+
+
+
+
+
+
+
+
+
+
