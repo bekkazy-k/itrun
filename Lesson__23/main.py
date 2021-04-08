3728a932c3f28fc28edfa4b2b347b085ef8fc735
Daniiar <akimovdaniiar010@gmail.com>
2021-04-08 20:22:16 +0600

    Update main.py

--- Lesson__23/main.py
+++ Lesson__23/main.py
@@ -1,33 +1,37 @@
-my_list = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+# my_list = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# print(my_list[0][1])
+
+#
+# def find_max(matrix):
+#     s = matrix[0][0]
+#     for i in range(len(matrix)):
+#         for j in range(len(matrix[i])):
+#             if s < matrix[i][j]:
+#                 s = matrix[i][j]
+#     return s
+# # print(find_max(my_list))
+#
+# def find_min(matrix):
+#     s = matrix[0][0]
+#     for i in range(len(matrix)):
+#         for j in range(len(matrix[i])):
+#             if s > matrix[i][j]:
+#                 s = matrix[i][j]
+#     return s
+# # print(find_min(my_list))
+#
+# def find_summa(matrix):
+#     s = 0
+#     for i in range(len(matrix)):
+#         for j in range(len(matrix[i])):
+#             s += matrix[i][j]
+#
+#     return s
+# # print(find_summa(my_list))
+#
 
 
-def find_max(matrix):
-    s = matrix[0][0]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if s < matrix[i][j]:
-                s = matrix[i][j]
-    return s
-# print(find_max(my_list))
-
-def find_min(matrix):
-    s = matrix[0][0]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if s > matrix[i][j]:
-                s = matrix[i][j]
-    return s
-# print(find_min(my_list))
-
-def find_summa(matrix):
-    s = 0
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            s += matrix[i][j]
-
-    return s
-# print(find_summa(my_list))
